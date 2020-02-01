f0face359aa394e1075e77bf6f1a84d546ab7839
VioletTurtle <violetturtle2002@gmail.com>
2020-02-01 15:58:09 -0500

    Added timer scripts and colliders

---
+++ UnityProject/GameJame2020/Assets/Scripts/Colliders/PartsScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartsScript : Collision
+{
+    bool isVisible = true;
+    bool addsTime = true;
+    bool isPickupable = true;
+
+    private override void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isVisible)
+        {
+            if (collision.gameObject.CompareTag(object_))
+            {
+                AddTime();
+                // Remove from parts array
+                // Destroy object
+            }
+        }
+    }
+
+    void AddTime()
+    {
+        // Add time to timer
+    }
+}
