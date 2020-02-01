84c0c14f744cdedcf7677ceeaa2c2c2e3da2d9e3
Maniac567 <46767818+Maniac567@users.noreply.github.com>
2020-02-01 13:56:48 -0500

    added collision script

---
+++ UnityProject/GameJame2020/Assets/Scripts/Collision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    [SerializeField]
+    bool isVisible = false;
+    bool addsTime = false;
+    bool isPickupable = false;
+    public string object_;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isVisible)
+        {
+            if (collision.gameObject.CompareTag(object_))
+            {
+                return;
+            }
+        }
+    }
+}
